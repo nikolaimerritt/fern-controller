29d2863868b9a3205c326182d40b479fda898053
ScratchGuru46 <alexeimerritt999@gmail.com>
2024-04-13 16:55:51 +0100

    Arduino server shows temp, humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,23 @@
 #include "main.h"
 
 ArduinoLEDMatrix led_matrix;
+SHT31 temp_humidity_sensor;
 
-byte frame_on[8][12] = {
-  { 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 },
-  { 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0 },
-  { 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
-  { 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
-  { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-};
-
-byte frame_off[8][12] = {0};
-
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key index number (needed only for WEP)
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS; 
+int keyIndex = 0;
 
 int led =  LED_BUILTIN;
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
+float temperature, humidity;
+unsigned long last_read_time_ms = 0;
+
 void setup() {
   Serial.begin(9600);   
   led_matrix.begin();
 
-  // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
     while (true);
@@ -38,85 +28,88 @@ void setup() {
     Serial.println("Please upgrade the firmware");
   }
 
-  // attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to Network named: ");
     Serial.println(ssid);
 
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
-    // wait 10 seconds for connection:
     delay(10000);
   }
-  server.begin();                          
-  printWifiStatus();                        
+    server.begin();       
+    Wire.begin();
+    Wire.setClock(WIRE_CLOCK);
+
+  temp_humidity_sensor.begin();                   
+  print_wifi_status();                        
 }
 
 
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {                             // if you get a client,
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.print(c);                    // print it out to the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-        Serial.flush();
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/H\">here</a> turn the LED on<br></p>");
-            client.print("<p style=\"font-size:7vw;\">Click <a href=\"/L\">here</a> turn the LED off<br></p>");
-            
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        if (currentLine.endsWith("GET /H")) {
-            Serial.println("[Received /H]");
-          led_matrix.renderBitmap(frame_on, 8, 12);
-        }
-        if (currentLine.endsWith("GET /L")) {
-            Serial.println("[Received /L]");
-          led_matrix.renderBitmap(frame_off, 8, 12);
-        }
-      }
-      
+    unsigned long ms_since_arduino_started = millis();
+    unsigned long ms_since_last_read = ms_since_arduino_started - last_read_time_ms;
+    if (last_read_time_ms == 0 || ms_since_last_read > 5000) {
+        read_temperature_humidity(temperature, humidity);
+        last_read_time_ms = millis();
     }
-    client.stop();
-  }
+
+    WiFiClient client = server.available();
+    if (client) {
+        String requestLine = "";
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                Serial.print(c);
+                if (c == '\n') {
+                    if (requestLine.length() == 0) {
+                        respond_to_client(client);
+                        break;
+                    } else {
+                        Serial.flush();
+                        requestLine = "";
+                    }
+                } else if (c != '\r') {
+                    requestLine += c;
+                }
+            }
+        }
+        client.stop();
+    }
 }
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
+void print_wifi_status() {
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
   long rssi = WiFi.RSSI();
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
-  // print where to go in a browser:
+
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
+}
+
+void read_temperature_humidity(float& temperature, float& humidity) {
+    temp_humidity_sensor.read(false);
+    temperature = temp_humidity_sensor.getTemperature();
+    humidity = temp_humidity_sensor.getHumidity();
+}
+
+void respond_to_client(WiFiClient& client) {
+    for (int i = 0; RESPONSE_TEMPLATE[i] != '\0'; i++) {
+        if (RESPONSE_TEMPLATE[i] == '$') {
+            if (RESPONSE_TEMPLATE[i + 1] == 'T') {
+                client.print(temperature);
+            } else if (RESPONSE_TEMPLATE[i + 1] == 'H') {
+                client.print(humidity);
+            }
+            i++;
+        } else {
+            client.print(RESPONSE_TEMPLATE[i]);
+        }
+    }
 }
--- src/main.h
+++ src/main.h
@@ -2,10 +2,38 @@
 #define __MAIN_H_INCLUDE
 
 #include <Arduino.h>
+#include <Wire.h>
 #include "Arduino_LED_Matrix.h"
 #include "arduino_secrets.h"
 #include "WiFiS3.h"
+#include "SHT31.h"
 
-void printWifiStatus();
+#define WIRE_CLOCK 100000
+
+const byte FRAME_ON[8][12] = {
+  { 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0 },
+  { 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0 },
+  { 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+const byte FRAME_OFF[8][12] = {0};
+
+#define NUM_RESPONSE_LINES 5
+
+const char* const RESPONSE_TEMPLATE = "HTTP/1.1 200 OK\n"
+    "Content-type:text/html\n"
+    "\n"
+    "<p> Temperature: $TC </p>"
+    "<p> Humidity: $H% </p>"
+;
+
+void print_wifi_status();
+void read_temperature_humidity(float& temperature, float& humidity);
+void respond_to_client(WiFiClient& client);
 
 #endif
